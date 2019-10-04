b0b4fe961d86fea0b15125ac93b79ecffdf9fd63
nchen0 <nicky.chen0@gmail.com>
2019-10-04 10:23:14 -0400

    add links

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import React from "react";
 import "./App.css";
 import SearchResults from "./SearchResults";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div class="container">
-          <div class="title">Bookfinder</div>
-          <div class="search"></div>
-          <SearchResults />
-        </div>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      books: ""
+    };
+  }
+
+  inputSearch = e => {
+    e.preventDefault();
+    this.setState({ input: e.target.value });
+  };
+
+  submitSearch = e => {
+    e.preventDefault();
+    axios
+      .get(
+        `https://www.googleapis.com/books/v1/volumes?q=${this.state.input}&key=${process.env.REACT_APP_API_KEY}`
+      )
+      .then(result => {
+        this.setState({ books: result.data.items, input: "" });
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div class="container">
+            <div class="title">Bookfinder</div>
+            <form onSubmit={this.submitSearch} class="search">
+              <input
+                value={this.state.input}
+                onChange={this.inputSearch}
+                placeholder="Type a book name"
+              />
+              <button>Search</button>
+            </form>
+            {this.state.books ? <SearchResults books={this.state.books} /> : null}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,39 +1,32 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./SearchResults.css";
-require("dotenv").config();
 
 class SearchResults extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       books: ""
     };
   }
-  componentDidMount() {
-    axios
-      .get(
-        `https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=${process.env.REACT_APP_API_KEY}`
-      )
-      .then(result => {
-        console.log("result: ", result.data.items);
-        this.setState({ books: result.data.items });
-      });
-  }
   render() {
+    console.log("this.props.books: ", this.props.books);
+    const books = this.props.books;
     return (
       <div>
-        {this.state.books
-          ? this.state.books.map(book => {
+        {books
+          ? books.map(book => {
               return (
-                <div key={book.id} className="book">
+                <a key={book.id} className="book" href={book.volumeInfo.canonicalVolumeLink}>
                   <div className="title">{book.volumeInfo.title}</div>
-                  <div className="author">{book.volumeInfo.authors.map(author => author)}</div>
+                  {book.volumeInfo.authors ? (
+                    <div className="author">{book.volumeInfo.authors.map(author => author)}</div>
+                  ) : null}
                   <div className="publishedDate">{book.volumeInfo.publishedDate}</div>
                   {book.volumeInfo.imageLinks ? (
                     <img src={book.volumeInfo.imageLinks.thumbnail} />
                   ) : null}
-                </div>
+                </a>
               );
             })
           : null}
